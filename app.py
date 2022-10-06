d2652099b9b37a914b06905bc5f07053efb93695
Jaime David Daza Tovar <jtovard@uninorte.edu.co>
2022-10-06 17:18:59 -0500

    Generación de proyecto Flask

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/conocenos')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/destinos')
+def travel():
+    return render_template('travel.html')
+
+
+@app.route("/registro")
+def registro():
+    return render_template('user_register.html')
+
+
+@app.route('/contacto')
+def contacto():
+    return render_template('atencion_usuario.html')
+
+
+@app.route("/reservas")
+def reservas():
+    return render_template('reservas.html')

c54aa4b78b265331246b245c1c3dd6e3effdcef0
Jaime David Daza Tovar <jtovard@uninorte.edu.co>
2022-10-13 14:06:12 -0500

    Validaciones de usuario y mensajes de error

--- gestion_vuelos/routes/auth.py
+++ gestion_vuelos/routes/auth.py
@@ -1,6 +1,7 @@
-from datetime import datetime
+from datetime import date, datetime
 from flask import Blueprint, request, session, render_template, redirect, url_for, flash, g
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.datastructures import ImmutableMultiDict
 from gestion_vuelos.models.ModelUsuario import ModelUsuario
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
@@ -13,21 +14,26 @@ def login():
 
     if email == None:
         flash("El correo es requerido", "error")
+        return redirect(request.referrer)
     elif password == None:
         flash("La contraseña es requerida", "error")
+        return redirect(request.referrer)
 
     password = str(password)
 
     user = ModelUsuario.obtener_por_correo(email)
     if user == None:
         flash("Credenciales incorrectas", "error")
+        return redirect(request.referrer)
     elif user[1] != email or not check_password_hash(user[2], password):
         flash("Credenciales incorrectas", "error")
+        return redirect(request.referrer)
     else:
         session.clear()
         session["user_id"] = user[0]
         session["user_nombre"] = user[3]
         return redirect(url_for("home.index"))
+
 
 @auth.route('/registro/inicial', methods=["POST"])
 def registro_inicial():
@@ -40,25 +46,22 @@ def registro_inicial():
 
 @auth.route("/register", methods=["POST"])
 def register():
-    nombre = request.form.get("fname", "")
-    apellido = request.form.get("lname", "")
-    tipo_documento = int(request.form.get("tipoDocumento", ""))
-    genero = request.form.get("genero", "")
-    identificacion = request.form.get("numeroIdentificacion", "")
+    if not validate_register_data(request.form):
+        return redirect(request.referrer)
+    nombre = request.form.get("fname") or ""
+    apellido = request.form.get("lname") or ""
+    tipo_documento = int(request.form.get("tipoDocumento") or "")
+    genero = request.form.get("genero") or ""
+    identificacion = request.form.get("numeroIdentificacion") or ""
     fecha_de_nacimiento = datetime.fromisoformat(
-        request.form.get("fechaNacimiento", ""))
-    nacionalidad = int(request.form.get("nacionalidad", ""))
-    correo = request.form.get("email", "")
-    email_confirm = request.form.get("email_confirm", correo)
-    password = generate_password_hash(request.form.get("password", ""))
-    password_confirm = request.form.get("password_confirm", password)
-    telefono = request.form.get("phone", "")
-    address = request.form.get("address")
-    pais = int(request.form.get("country", ""))
-    ciudad = int(request.form.get("city", ""))
-    contact = request.form.get("contact")
-    n_contact = request.form.get("n_contact")
-    aceptar_politicas = request.form.get("aceptar_politicas")
+        request.form.get("fechaNacimiento") or "")
+    nacionalidad = int(request.form.get("nacionalidad") or "")
+    correo = request.form.get("email") or ""
+    password = generate_password_hash(request.form.get("password") or "")
+    telefono = request.form.get("phone") or ""
+    pais = int(request.form.get("country") or "")
+    ciudad = int(request.form.get("city") or "")
+
     usuario = ModelUsuario.obtener_por_correo(correo)
     if usuario is None:
         ModelUsuario.agregar_usuario(tipo_documento, identificacion, nombre, apellido,
@@ -71,6 +74,71 @@ def register():
     return redirect(url_for("home.index"))
 
 
+def validate_register_data(form: ImmutableMultiDict):
+    validated = True
+    if not form.get("fname"):
+        flash("El nombre es requerido", "error")
+        validated = False
+    elif not form.get("lname"):
+        flash("El apellido es requerido", "error")
+        validated = False
+    elif not form.get("tipoDocumento"):
+        flash("El tipo de documento de identidad es requerido es requerido", "error")
+        validated = False
+    elif not form.get("genero"):
+        flash("El género es requerido", "error")
+        validated = False
+    elif not form.get("numeroIdentificacion"):
+        flash("El número de identificación es requerido", "error")
+        validated = False
+    elif not form.get("fechaNacimiento"):
+        flash("La fecha de nacimiento es requerida", "error")
+        validated = False
+    elif not form.get("nacionalidad"):
+        flash("La nacionalidad es requerida", "error")
+        validated = False
+    elif not form.get("email"):
+        flash("El correo es requerido", "error")
+        validated = False
+    elif not form.get("email_confirm"):
+        flash("Debe escribir nuevamente su correo", "error")
+        validated = False
+    elif form.get("email") != form.get("email_confirm"):
+        flash("Los correos no coinciden", "error")
+        validated = False
+    elif not form.get("password"):
+        flash("La contraseña es requerida", "error")
+        validated = False
+    elif not form.get("password_confirm"):
+        flash("Debe escribir nuevamente su contraseña", "error")
+        validated = False
+    elif form.get("password") != form.get("password_confirm"):
+        flash("Las contraseñas no coinciden", "error")
+        validated = False
+    elif not form.get("phone"):
+        flash("El teléfono de contacto es requerido", "error")
+        validated = False
+    elif not form.get("address"):
+        flash("La dirección es requerida", "error")
+        validated = False
+    elif not form.get("country"):
+        flash("El país es requerido", "error")
+        validated = False
+    elif not form.get("city"):
+        flash("La ciudad es requerida", "error")
+        validated = False
+    elif not form.get("contact"):
+        flash("El nombre del contacto de emergencia es requerido", "error")
+        validated = False
+    elif not form.get("n_contact"):
+        flash("El teléfono del contacto de emergencia es requerido", "error")
+        validated = False
+    elif not form.get("aceptar_politicas"):
+        flash("Debe aceptar las políticas de privacidad", "error")
+        validated = False
+    return validated
+
+
 @auth.route('/logout', methods=["GET"])
 def logout():
     session.clear()
@@ -79,7 +147,7 @@ def logout():
 
 @auth.before_app_request
 def load_logged_in_user():
-    if session :
+    if session:
         user_id = session.get('user_id')
         if user_id is None:
             g.user = None
